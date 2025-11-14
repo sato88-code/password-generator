e8948662a0d39cac44d40e80f86e4eaac94c5387
sato88-code <stndvd05c27d086u@studenti.unical.it>
2025-11-14 02:33:23 +0100

    validazione input  e logica ottimizzata aggiunta allo script

---
+++ main.py
@@ -0,0 +1,46 @@
+#importo le librerie che mi serviranno
+import string 
+import random
+import time
+
+# creo il mio insieme da cui andremo a "pescare" i caratteri che formeranno la nostra password e la password finale vuota
+caratteri = string.ascii_letters + string.digits 
+password_finale = ""
+#do il benvenuto all'utente spiegandogli cosa fa lo script e la lunghezza della sua password
+print("Benvenuto, sono password-generator!")
+time.sleep(0.30)
+print("so che hai bisogno di una nuova password totalmente sicura, sei nel posto giusto")
+lunghezza = input("quanti caratteri vuoi che sia lunga la tua password?\n")
+lunghezza = int(lunghezza)
+
+# chiediamo all'utente se vuole inserire nella sua password anche dei caratteri speciali
+time.sleep(0.30)
+print("perfetto!\n vuoi che nella tua password ci siano anche dei caratteri speciali?")
+
+# in base alla risposta ci muoviamo in modo diverso
+time.sleep(0.30)
+# se l'utente vuole caratteri speciali nella sua password
+while True:
+    risposta = input("si/no \n").strip().lower()
+    if risposta == "si":
+        caratteri = caratteri + string.punctuation
+        print("perfetto aggiungo i caratteri speciali")
+        break
+# se l'utente NON vuole caratteri speciali nella sua password
+    elif risposta == "no":
+        print("ok,ti genero una password senza caratteri speciali")
+        break
+# se l'utente ha dato una risposta diversa dal si o dal no 
+    else:
+        print("ERRORE:puoi rispondere con si o con no. riprova!")
+    
+
+print("perfetto inizio subito a creare la tua password!")
+while len(password_finale)<lunghezza:
+    password_finale = password_finale + random.choice(caratteri)
+for i in range(3):
+    time.sleep(0.30)
+    print(".",end='',flush=True)
+time.sleep(1)
+print("\n perfetto ecco la tua password: \n",password_finale)
+
